b854109a3c94c569e475919116e179c2010a98d5
Neil A. Ernst <neil@neilernst.net>
2016-06-21 10:57:51 -0400

    more copyright changes

--- edu.cmu.sei.eebm.tests/xtend-gen/edu/cmu/sei/eebm/tests/EEBMParserTest.java
+++ edu.cmu.sei.eebm.tests/xtend-gen/edu/cmu/sei/eebm/tests/EEBMParserTest.java
@@ -1,3 +1,25 @@
+/**
+ * EEBM
+ * Copyright (c) 2016 Carnegie Mellon University.
+ * All Rights Reserved.
+ * 
+ * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE
+ * MATERIAL IS FURNISHED ON AN “AS-IS” BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO
+ * WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING,
+ * BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY,
+ * OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
+ * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK,
+ * OR COPYRIGHT INFRINGEMENT.
+ * 
+ * Released under a BSD-style license, please see COPYRIGHT or contact
+ * permission@sei.cmu.edu for full terms.  DM-0003721
+ * 
+ * This Software includes and/or makes use of the following
+ * Third-Party Software subject to its own license:
+ * a.       Eclipse libraries (https://eclipse.org/)
+ * b.      Xtext https://eclipse.org/Xtext/
+ * Complete information can be found in build.xml
+ */
 package edu.cmu.sei.eebm.tests;
 
 import com.google.inject.Inject;
